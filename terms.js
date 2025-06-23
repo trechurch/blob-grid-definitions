8c5618554b412cf34cd5ffe83c3dcd2f7de93d1d
trechurch <trechurch@gmail.com>
2025-06-23 17:59:43 -0500

    Create terms.js

---
+++ terms.js
@@ -0,0 +1,18 @@
+const terms = [
+  {
+    term: "Flux",
+    definitions: [
+      "A state of constant change.",
+      "A physical flow rate.",
+      "In electronics, magnetic field flow."
+    ]
+  },
+  {
+    term: "Node",
+    definitions: [
+      "A connection point.",
+      "A data structure element.",
+      "A point of intersection."
+    ]
+  }
+];
